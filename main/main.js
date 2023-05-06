3fd2489d1bd2f78ac9d22ff0a3a3c773ad7f5fca
hburaks <hasanburaksongur@gmail.com>
2023-05-06 23:22:32 +0300

    Create scroll animation and change active menu

--- main/main.js
+++ main/main.js
@@ -11,12 +11,39 @@ toggleMenu("nav-open", "nav-menu");
 toggleMenu("nav-close", "nav-menu");
 
 // active menu
-const navLink = document.querySelectorAll(".nav__link");
-function linkAction() {
-	navLink.forEach((n) => n.classList.remove("active"));
-	this.classList.add("active");
+const sections = document.querySelectorAll("section[id]");
 
-	const navMenu = document.getElementById("nav-menu");
-	navMenu.classList.remove("show");
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute("id");
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector(".nav__menu a[href*=" + sectionId + "]")
+				.classList.add("active");
+		} else {
+			document
+				.querySelector(".nav__menu a[href*=" + sectionId + "]")
+				.classList.remove("active");
+		}
+	});
 }
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+window.addEventListener("scroll", scrollActive);
+// scroll reveal animation
+const sr = ScrollReveal({
+	origin: "top",
+	distance: "60px",
+	duration: 2000,
+	delay: 200
+});
+
+sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
+sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
+	delay: 400
+});
+sr.reveal(".home__social-icon", { interval: 200 });
+sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
