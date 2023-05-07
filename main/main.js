17c6ec850e258828870435de3f3165067e45299e
hburaks <hasanburaksongur@gmail.com>
2023-05-07 10:25:32 +0300

    Close mobile nav menu when active selected

--- main/main.js
+++ main/main.js
@@ -9,6 +9,14 @@ const toggleMenu = (toggleId, navId) => {
 };
 toggleMenu("nav-open", "nav-menu");
 toggleMenu("nav-close", "nav-menu");
+// close mobile nav menu when selected
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // active menu
 const sections = document.querySelectorAll("section[id]");
